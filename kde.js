fe9e4ac8f648361ec0e6aa7e1eee0225b348823a
gilpasternak35 <gilpasternak35@gmail.com>
2021-09-06 16:20:16 -0700

    Day 1 JavaScript strructure set up

---
+++ kde.js
@@ -0,0 +1,35 @@
+function prompt_user() {
+// Function prompting user for a list of data
+
+ var data = prompt("Please enter a list of data points");
+
+ // Logging to Console
+ console.log(data);
+ const myArr = data.split(",");
+
+ // Displaying list of split data
+ document.getElementById("Displayer").innerHTML = myArr;
+}
+
+function compute_kde()
+{
+	var current_data = document.getElementById("Displayer");
+	const transformed_data = new Array();
+
+	/// Traversal and transformation fo elements
+	for(var i = 0; i < current_data.length; i++)
+	{
+		// pushing elements onto the array
+		transformed_data.push(parseInt(current_data[i]) * 2);
+	}
+
+	// Changing HTML
+	console.log(transformed_data)
+	document.getElementById('Display_KDE').innerHTML = transformed_data;
+
+}
+
+function plot_kde()
+{
+
+}
